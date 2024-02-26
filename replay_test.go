fd09f3648a01663f7d24a2ec22e42e33c62cbb4f
Trond Hindenes <trond@hindenes.com>
2024-02-26 21:51:20 +0100

    fixed tests

--- replay_test.go
+++ replay_test.go
@@ -1,28 +1,23 @@
-package plugindemo_test
+package traefikreplay_test
 
 import (
 	"context"
-	"io"
+	"github.com/trondhindenes/traefikreplay"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-
-	"github.com/trondhindenes/traefik-replay"
 )
 
 func TestDemo(t *testing.T) {
-	cfg := plugindemo.CreateConfig()
-	cfg.Headers["X-Host"] = "[[.Host]]"
-	cfg.Headers["X-Method"] = "[[.Method]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-Demo"] = "test"
+	cfg := traefikreplay.CreateConfig()
+	cfg.ReplayUrl = "http://localhost:8080"
+	cfg.Percentage = 0
 
 	ctx := context.Background()
 	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
 
-	handler, err := plugindemo.New(ctx, next, cfg, "demo-plugin")
+	handler, err := traefikreplay.New(ctx, next, cfg, "demo-plugin")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -41,25 +36,4 @@ func TestDemo(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	assertHeader(t, req, "X-Host", "localhost")
-	assertHeader(t, req, "X-URL", "http://localhost")
-	assertHeader(t, req, "X-Method", "GET")
-	assertHeader(t, req, "X-Demo", "test")
-	assertBody(t, req, bodyString)
-}
-
-func assertHeader(t *testing.T, req *http.Request, key, expected string) {
-	t.Helper()
-
-	if req.Header.Get(key) != expected {
-		t.Errorf("invalid header value: %s", req.Header.Get(key))
-	}
-}
-
-func assertBody(t *testing.T, req *http.Request, bodyString string) {
-	t.Helper()
-	body, _ := io.ReadAll(req.Body)
-	if string(body) != bodyString {
-		t.Errorf("unexpected body")
-	}
 }
